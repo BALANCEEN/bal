fb7f61d09171836189f304482d7bf5c08173a24f
BALANCEEN <1749578380@qq.com>
2019-07-17 12:30:27 +0800

    的

--- src/aaa/Test.java
+++ src/aaa/Test.java
@@ -3,9 +3,9 @@ package aaa;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(112222);
-
-		System.out.println(33322);
-
-
+		System.out.println(1211);
+		System.out.println(222);
+		System.out.println(222);
+		System.out.println(33);
 	}
 }
